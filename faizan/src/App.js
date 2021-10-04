8a1c4338a14a37fe4e73260793a8a5142582a266
skfaizan888 <skfaizana@gmail.com>
2021-10-04 07:32:33 -0700

    finish

--- faizan/src/App.js
+++ faizan/src/App.js
@@ -1,12 +1,13 @@
 // import { Home } from "./Components/Home";
 // import { Nav } from "./Components/Nav";
 // import { Footer } from "./Components/Home/Footer";
-import { Navi } from "./Components/Navigation/Navi";
+// import { Navi } from "./Components/Navigation/Navi";
 import { Service } from "./Components/Pages/Service";
 import { Contact } from "./Components/Pages/Contact";
 import { About } from "./Components/Pages/About";
-import { HomeFirst } from "./Components/HomeFirst/HomeFirst";
-import { Router,Switch,Route } from "react-router-dom";
+// import { HomeFirst } from "./Components/HomeFirst/HomeFirst";
+import { BrowserRouter as Router,Switch,Route } from "react-router-dom";
+import { NaviTwo } from "./Components/Navigation/NaviTwo";
 
 const App = () => {
   return (
@@ -16,12 +17,13 @@ const App = () => {
               {/*  <Home />
                <Footer /> */}
         <Router>
-        <Navi />
+        <NaviTwo />
         <Switch>
          <Route path="/Service"><Service/></Route>
+         <Route path="/Menu"><Menu/></Route>
          <Route path="/Contact"><Contact/></Route>
-         <Route path="/About"><About/></Route>
-         <Route path="/"><HomeFirst/></Route>
+         <Route path="/About"><About /></Route>
+         <Route path="/"><React-Bootstrap/></Route>
          
        </Switch>
         </Router>
---
+++ faizan/src/Components/Navigation/NaviTwo.jsx
@@ -0,0 +1,22 @@
+import {Navbar} from "react-Bootstrap"
+import { NavLink } from "react-router-dom"
+
+
+export const NaviTwo=()=>{
+    return(
+        <Navbar bg="light" expand="lg">
+  <Container>
+    <NavbarBrand to="/">React-Bootstrap</NavbarBrand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <NavLink to="Service">Service</NavLink>
+        <NavLink to="Contact">Contact</NavLink>
+        <NavLink to="Menu">Menu</NavLink>
+        <NavLink to="About">About </NavLink>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+    )
+}
