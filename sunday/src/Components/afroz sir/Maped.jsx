11dc45f0670186cb4b5a604819567646de753c55
skfaizan888 <skfaizana@gmail.com>
2021-10-08 02:42:16 -0700

    maping-practice

--- sunday/src/Components/afroz sir/Maped.jsx
+++ sunday/src/Components/afroz sir/Maped.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {person} from "./Maping";
 
 export const Maped=() => {
-    const data = person.data;
+    const data  = person.data;
     const data2 = data.filter ((item) => item.age >=25);
     console.log("checking...." ,data);
     return(
@@ -10,14 +10,15 @@ export const Maped=() => {
         <h1>Persona Details</h1>
         
         {data2.map((item)=>{
-            return(
+            return(    
             <div>
             <h5>{item.name}</h5>
-            <h5>{item.age}</h5>
-             <h5>{item.city}</h5>
+            <h5>{item.age} </h5>
+            <h5>{item.city}</h5>
           </div> 
             )
     })  }
     </div>
     ); 
 };
+
